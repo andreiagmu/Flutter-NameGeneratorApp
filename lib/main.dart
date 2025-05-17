f6c159a2acbb99bad0a66c218300092e858fb70a
Andrei Müller (Andy Miira) <andreiagmu@gmail.com>
2025-05-17 20:50:01 -0300

    MyHomePage: Add mobile-friendly layout

--- lib/main.dart
+++ lib/main.dart
@@ -98,42 +98,68 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
 
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-          body: Row(
-            children: [
-              SafeArea(
-                child: NavigationRail(
-                  extended: constraints.maxWidth >= 600,
-                  destinations: [
-                    NavigationRailDestination(
-                        icon: Icon(Icons.home),
-                        label: Text('Home')
-                    ),
-                    NavigationRailDestination(
-                        icon: Icon(Icons.favorite),
-                        label: Text('Favorites')
-                    ),
-                  ],
-                  selectedIndex: selectedIndex,
-                  onDestinationSelected: (value) {
-                    setState(() {
-                      selectedIndex = value;
-                    });
-                  },
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 450) {
+            // Use a more mobile-friendly layout with BottomNavigationBar
+            // on narrow screens.
+            return Column(
+              children: [
+                Expanded(child: mainArea),
+                SafeArea(
+                  child: BottomNavigationBar(
+                    items: [
+                      BottomNavigationBarItem(
+                          icon: Icon(Icons.home),
+                          label: 'Home'
+                      ),
+                      BottomNavigationBarItem(
+                          icon: Icon(Icons.favorite),
+                          label: 'Favorites'
+                      ),
+                    ],
+                    currentIndex: selectedIndex,
+                    onTap: (value) {
+                      setState(() {
+                        selectedIndex = value;
+                      });
+                    },
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  child: page,
+              ],
+            );
+          }
+          else {
+            return Row(
+              children: [
+                SafeArea(
+                  child: NavigationRail(
+                    extended: constraints.maxWidth >= 600,
+                    destinations: [
+                      NavigationRailDestination(
+                          icon: Icon(Icons.home),
+                          label: Text('Home')
+                      ),
+                      NavigationRailDestination(
+                          icon: Icon(Icons.favorite),
+                          label: Text('Favorites')
+                      ),
+                    ],
+                    selectedIndex: selectedIndex,
+                    onDestinationSelected: (value) {
+                      setState(() {
+                        selectedIndex = value;
+                      });
+                    },
+                  ),
                 ),
-              ),
-            ],
-          ),
-        );
-      }
+                Expanded(child: mainArea),
+              ],
+            );
+          }
+        },
+      ),
     );
   }
 }
