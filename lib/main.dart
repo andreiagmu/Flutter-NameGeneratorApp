8699e0d667ec572e5d7f4ef884216689313a416f
Andrei Müller (Andy Miira) <andreiagmu@gmail.com>
2025-05-17 22:01:20 -0300

    main.dart: Add HistoryListView (WIP); Add _maskingGradient

--- lib/main.dart
+++ lib/main.dart
@@ -316,3 +316,31 @@ class FavoritesPage extends StatelessWidget {
     );
   }
 }
+
+class HistoryListView extends StatefulWidget {
+  const HistoryListView({super.key});
+
+  @override
+  State<HistoryListView> createState() => _HistoryListViewState();
+}
+
+class _HistoryListViewState extends State<HistoryListView> {
+  /// Needed so that [MyAppState] can tell [AnimatedList] below to animate
+  /// new items.
+  final _key = GlobalKey();
+
+  /// Used to "fade out" the history items at the top, to suggest continuation.
+  static const Gradient _maskingGradient = LinearGradient(
+    // This gradient goes from "fully transparent" to "fully opaque black"...
+    colors: [Colors.transparent, Colors.black],
+    // ...from the top (transparent) to half (0.5) of the way to the bottom.
+    stops: [0.0, 0.5],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
